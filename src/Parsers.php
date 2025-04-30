1b01b8c005fe6f7e875e5bd49449eb78a28021f7
fzxcvbn <diankahid@gmail.com>
2025-04-30 18:45:39 +0300

    update

--- src/Parsers.php
+++ src/Parsers.php
@@ -9,7 +9,8 @@ function convertingFile(string $fileContent, string $extension): object
     switch ($extension) {
         case 'json':
             return json_decode($fileContent);
-        case 'yaml' || 'yml':
+        case 'yaml':
+        case 'yml':
             return Yaml::parse($fileContent, Yaml::PARSE_OBJECT_FOR_MAP);
         default:
             throw new \Exception("Invalid extension. Extension must be in JSON, YAML or YML");
